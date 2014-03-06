6c11bf03c398c806a60968518eaae9d9908b106e
Kai Chu Chung <cage.chung@gmail.com>
2014-03-06 21:16:43 +0800

    pop bootstrap theme and UI feature add

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,78 @@
 'use strict';
+
+var background_page = chrome.extension.getBackgroundPage();
+var current_tab;
+
+function addActions(){
+    
+    var actions = [{
+       id: 'copyTitle',
+       name:'Title'
+    }, {
+       id: 'copyTitleUrl',
+       name: 'Title (URL)'
+    }, {
+       id: 'copyTitleUrlShortern',
+       name: 'Title (URL)',
+       small: ' Shortern'
+    },{
+        id:'copyUrl',
+        name: '(URL)',
+        small: ' Shortern'
+    }];
+    
+    var action_element = document.getElementById('actions');
+    var frag = document.createDocumentFragment();
+    
+    for (var key in actions) {
+        var action = actions[key];
+        
+        var anchor = document.createElement('a');
+        anchor.setAttribute('href', '#');
+        anchor.setAttribute('id', action.id);
+        anchor.setAttribute('class', 'list-group-item');
+        anchor.addEventListener('click', function (e) {
+            var id;
+            if(e.target.tagName === 'A')
+                id = e.target.id;
+            if(e.target.tagName === 'H5')
+                id =e.target.parentNode.id;
+            if(e.target.tagName === 'SMALL')
+                id =e.target.parentNode.parentNode.id;
+            
+            background_page.copyToClipboard(current_tab, id);
+            window.close();
+        }, false);
+        
+        var h5 = document.createElement('h5');
+        h5.innerHTML = action.name;
+                
+        var small = document.createElement('small');
+        small.innerHTML = action.small || '';
+        
+        if(action.small)
+            h5.appendChild(small);
+        
+        anchor.appendChild(h5);
+        
+        frag.appendChild(anchor);
+    }
+    
+    action_element.appendChild(frag);
+}
+
+function init()
+{
+	chrome.tabs.getSelected(null, function(tab) 
+	{
+		current_tab = tab;
+		//background_page.shortenUrl(tab.url, tab.incognito, onResponse);
+	});
+    
+    addActions();
+}
+
+document.addEventListener('DOMContentLoaded', function () 
+{
+	init();
+});
